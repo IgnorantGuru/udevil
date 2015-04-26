341ddd3966e58f2d4ec55c500f61af3e57c5df73
IgnorantGuru <ignorantguru@gmx.com>
2015-04-26 09:34:56 -0600

    [git-glob] replace modified is_glob_special with original #51

--- src/wildmatch.c
+++ src/wildmatch.c
@@ -54,6 +54,42 @@ typedef unsigned char uchar;
 #define ISUPPER(c) (ISASCII(c) && isupper(c))
 #define ISXDIGIT(c) (ISASCII(c) && isxdigit(c))
 
+// free is_glob_special from git tree
+#define GIT_GLOB_SPECIAL 0x08
+const unsigned char sane_ctype[256];
+#define GIT_SPACE 0x01
+#define GIT_DIGIT 0x02
+#define GIT_ALPHA 0x04
+#define GIT_GLOB_SPECIAL 0x08
+#define GIT_REGEX_SPECIAL 0x10
+#define GIT_PATHSPEC_MAGIC 0x20
+#define GIT_CNTRL 0x40
+#define GIT_PUNCT 0x80
+enum {
+S = GIT_SPACE,
+A = GIT_ALPHA,
+D = GIT_DIGIT,
+G = GIT_GLOB_SPECIAL, /* *, ?, [, \\ */
+R = GIT_REGEX_SPECIAL, /* $, (, ), +, ., ^, {, | */
+P = GIT_PATHSPEC_MAGIC, /* other non-alnum, except for ] and } */
+X = GIT_CNTRL,
+U = GIT_PUNCT,
+Z = GIT_CNTRL | GIT_SPACE
+};
+const unsigned char sane_ctype[256] = {
+X, X, X, X, X, X, X, X, X, Z, Z, X, X, Z, X, X, /* 0.. 15 */
+X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, /* 16.. 31 */
+S, P, P, P, R, P, P, P, R, R, G, R, P, P, R, P, /* 32.. 47 */
+D, D, D, D, D, D, D, D, D, D, P, P, P, P, P, G, /* 48.. 63 */
+P, A, A, A, A, A, A, A, A, A, A, A, A, A, A, A, /* 64.. 79 */
+A, A, A, A, A, A, A, A, A, A, A, G, G, U, R, P, /* 80.. 95 */
+P, A, A, A, A, A, A, A, A, A, A, A, A, A, A, A, /* 96..111 */
+A, A, A, A, A, A, A, A, A, A, A, R, R, U, P, X, /* 112..127 */
+/* Nothing in the 128.. range */
+};
+#define sane_istest(x,mask) ((sane_ctype[(unsigned char)(x)] & (mask)) != 0)
+#define is_glob_special(x) sane_istest(x,GIT_GLOB_SPECIAL)
+/*
 static int is_glob_special(char c)
 {
 	static const char globs[] = { '$', '(', ')', '+', '.', '^', '{', '|' };
@@ -64,7 +100,7 @@ static int is_glob_special(char c)
 	}
 	return 0;
 }
-
+*/
 
 /* Match pattern "p" against "text" */
 static int dowild(const uchar *p, const uchar *text, unsigned int flags)
